a4b42f197f9ad4257e82e374db1c20cab0029ba2
davidaleksejevs <david.aleksejevs@gmail.com>
2022-05-30 12:02:10 +0100

    dynamic user template

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,12 @@ router.get("/new", (req, res) => {
   res.send("User new form");
 });
 
+router.post("/", (req, res) => {
+  res.send("user created");
+});
+
+router.get('/"id', (req, res) => {
+  res.send(`Get user with ID ${req.params.id}`);
+});
+
 module.exports = router;
