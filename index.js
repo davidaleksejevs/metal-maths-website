2bd5e3bc16d8740dd93ec33ed7f988a80664b0dd
davidaleksejevs <david.aleksejevs@gmail.com>
2022-05-31 11:48:50 +0100

    adding some logic `
    added random numbers and displayed it on the page

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+//defining stuff idk?
 const express = require("express");
 const app = express();
 const port = 3000;
-//
+// random shit that has nothing to do with the routes
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+//in here is functions that i will use
+
+function randNum(digits) {
+  const num = Math.floor(Math.random() * digits);
+  return num;
+}
 //
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/practice", (req, res) => {
-  res.render("practice");
-  console.log("someones going to practice!!!");
+  const num1 = randNum(100);
+  const num2 = randNum(100);
+  res.render("practice", { num1, num2 });
 });
 
 const userRouter = require("./routes/users");
